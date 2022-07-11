bc0725797b81cfc17f25708a6078eaeff05055ed
lenovo <radheshyamb9@gmail.com>
2022-07-11 21:32:47 +0530

    ADDED : BookStore Backend Connect

--- src/Service/BookStoreService.js
+++ src/Service/BookStoreService.js
@@ -1,22 +1,14 @@
 import axios from "axios";
-const BASE_URL = 'http://localhost:8085/BookStore';
+const BASE_URL = 'http://localhost:8085/book';
 
 class BookStoreService {
 
   getAllBooks() {
     return axios.get(`${BASE_URL}/getAll`);
   }
-  getBook(BookId) {
-    return axios.get(`${BASE_URL}/getBy/${BookId}`);
-  }
-  getContactCount() {
-    return axios.get(`${BASE_URL}/getcount`);
-  }
-  getsortBook() {
-    return axios.put(`${BASE_URL}/sortascending`);
-  }
-  getsortBooks() {
-    return axios.put(`${BASE_URL}/sortdescending`);
+
+  insertBook() {
+    return axios.get(`${BASE_URL}/Insert`);
   }
 
 }
--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -3,185 +3,43 @@
       <div>
         <v-card-title>Book ({{ count }}Items)</v-card-title>
       </div>
-    <v-card class="mt-5 mb-8">
-      <v-card-text>
-        <v-container>
-          <v-row>
-            <v-col cols="3">
-              <v-card
-                class="pa-2"
-                outlined
-                tile
-              >
-              <v-img  height="150"
-                      width="150"
-                      src="../assets/Image 10.png"></v-img>
-                Ux-Design
-                 <v-card-actions>
-                    <v-btn
-                        color="#a03037" dark
-                        outline=""
-                       @click="reserve"
-                    >
-                            Add to Cart
-                    </v-btn>
-                  </v-card-actions>
-              </v-card>
+     <v-container fluid class="card">
+        <v-row >
+            <v-col cols="3"  v-for="BookInfos in BookInfo" :key="BookInfos.bookId">
+           <v-card>
+              <v-img
+                :src="BookInfos.bookImage"
+                class="white--text align-end"
+                gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+                height="150px"
+                width="150px"
+              />
+              <v-card-title v-text="BookInfos.bookName"></v-card-title>
+              <v-card-text>
+                <div class="my-4 text-subtitle-1">
+                  by {{ BookInfos.authorName }}
+                </div>
+                <v-card-title>Price:Rs {{ BookInfos.price }}</v-card-title>
+              </v-card-text>
+
+              <v-row align="center" justify="space-around">
+                <v-btn
+                  color="#a03037"
+                  depressed
+                  elevation="2"
+                  small
+                  @click="AddtoCart(BookInfos.bookId, BookInfos.price)"
+                >
+                  <v-icon>mdi-cart</v-icon>Add To Cart
+                </v-btn>
+                <v-btn elevation="2" small text>
+                  <v-icon>mdi-cards-heart-outline</v-icon>WhishList</v-btn
+                >
+              </v-row>
+            </v-card>
             </v-col>
-            <v-col cols="3">
-              <v-card
-                class="pa-2"
-                outlined
-                tile
-              >
-              <v-img  height="150"
-                      width="150"
-                      src="../assets/Image 11.png"></v-img>
-                Ux-Design
-                <v-card-actions>
-                    <v-btn
-                        color="#a03037" dark
-                        outline=""
-                       @click="reserve"
-                    >
-                            Add to Cart
-                    </v-btn>
-                  </v-card-actions>
-              </v-card>
-            </v-col>
-            <v-col cols="3">
-              <v-card
-                class="pa-2"
-                outlined
-                tile
-              >
-              <v-img  height="150"
-                      width="150"
-                      src="../assets/Image 11.png"></v-img>
-                Ux-Design
-                <v-card-actions>
-                    <v-btn
-                        color="#a03037" dark
-                        outline=""
-                       @click="reserve"
-                    >
-                            Add to Cart
-                    </v-btn>
-                  </v-card-actions>
-              </v-card>
-            </v-col>
-            <v-col cols="3">
-              <v-card
-                class="pa-2"
-                outlined
-                tile
-              >
-              <v-img  height="150"
-                      width="150"
-                      src="../assets/Image 14.png"></v-img>
-                Ux-Design
-                <v-card-actions>
-                    <v-btn
-                        color="#a03037" dark
-                        outline=""
-                       @click="reserve"
-                    >
-                            Add to Cart
-                    </v-btn>
-                  </v-card-actions>
-              </v-card>
-            </v-col>
-          </v-row>
-          
-          <v-row>
-            <v-col cols="3">
-              <v-card
-                class="pa-2"
-                outlined
-                tile
-              >
-              <v-img  height="150"
-                      width="150"
-                      src="../assets/Image 11.png"></v-img>
-                Ux-Design
-                <v-card-actions>
-                    <v-btn
-                        color="#a03037" dark
-                        outline=""
-                       @click="reserve"
-                    >
-                            Add to Cart
-                    </v-btn>
-                  </v-card-actions>
-              </v-card>
-            </v-col>
-            <v-col cols="3">
-              <v-card
-                class="pa-2"
-                outlined
-                tile
-              >
-              <v-img  height="150"
-                      width="150"
-                      src="../assets/Image 11.png"></v-img>
-                Ux-Design
-                <v-card-actions>
-                    <v-btn
-                        color="#a03037" dark
-                        outline=""
-                       @click="reserve"
-                    >
-                            Add to Cart
-                    </v-btn>
-                  </v-card-actions>
-              </v-card>
-            </v-col>
-            <v-col cols="3">
-              <v-card
-                class="pa-2"
-                outlined
-                tile
-              >
-              <v-img  height="150"
-                      width="150"
-                      src="../assets/Image 18.png"></v-img>
-                Group
-                <v-card-actions>
-                    <v-btn
-                        color="#a03037" dark
-                        outline=""
-                       @click="reserve"
-                    >
-                            Add to Cart
-                    </v-btn>
-                  </v-card-actions>
-              </v-card>
-            </v-col>
-            <v-col cols="3">
-              <v-card
-                class="pa-2"
-                outlined
-                tile
-              >
-              <v-img  height="150"
-                      width="150"
-                      src="../assets/Image 7.png"></v-img>
-                Ux-Design
-                <v-card-actions>
-                    <v-btn
-                        color="#a03037" dark
-                        outline=""
-                       @click="reserve"
-                    >
-                            Add to Cart
-                    </v-btn>
-                  </v-card-actions>
-              </v-card>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-card-text>
-    </v-card>
+            </v-row>
+            </v-container>    
   </div>
 </template>
 
@@ -206,6 +64,51 @@ export default {
         { src: "../assets/Image 18.png", flex: 6 },
       ],
     };
+  },
+
+  methods: {
+    getBook() {
+      BookStoreService.getAllBooks().then((response) => {
+        console.log(response.data.data);
+        this.BookInfo = response.data.data;
+      });
+    },
+
+    InsertBook() {
+      BookStoreService.insertBook().then((response) => {
+        console.log(response.data.data);
+        this.BookInfo = response.data.data;
+      })
+    },
+    getContactCount() {
+      BookStoreService.getContactCount().then((response) => {
+        console.log(response.data.data);
+        this.count = response.data.data;
+      });
+    },
+    AddtoCart(id, total) {
+      console.log(this.BookInfo.bookId);
+      console.log(id);
+      this.formValues.bookId = id;
+      this.formValues.userId = 23;
+      this.formValues.quantity = 1;
+      this.formValues.total = total;
+      console.log(this.formValues);
+      const data = this.formValues;
+      CartService.addCart(data)
+        .then((response) => {
+          console.log(response);
+          // this.BookInfo = response.data.data;
+        })
+        .catch((error) => {
+          console.log(error);
+          alert("WARNING!! Error while adding the Added person !");
+        });
+    },
+  },
+  created() {
+    this.getBook();
+    this.getContactCount();
   },
 };
 </script>
